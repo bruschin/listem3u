360fc6dcb92bae8de4e178ec017d29d33c47d35a
BRUSCHI Nicolas <nicolas.bruschi@meteo.fr>
2023-04-02 13:10:45 +0200

    pb

--- devtools/lanceur_sphinx.bash
+++ devtools/lanceur_sphinx.bash
@@ -1,16 +1,18 @@
 #!/bin/bash
 ###########
-## genere rapport pylint
-## nécessite d'avoir excute
-## pylint --generate-rcfile > .pylintrc
+## genere documentation avec sphinx
+## nécessite d'avoir installé sphinx graphviz rhino
 ###########
 export PATH=$PATH:~/opt/bin
 REPTRAV="$(dirname $0)"
 cd "docs" || exit 1
 
-sphinx-build . _build
+make -C ./ clean
+#sphinx-build . _build
 sphinx-apidoc -f -o ./ ../src
-sphinx-multiversion ./ _build/html
-make clean html
+#sphinx-multiversion ./ _build/html
+sphinx-build -b html ./ ./_build/html -D language=fr
+#sphinx-build -b rinoh ./ ./_build/rinoh -D language=fr
+sphinx-build -b epub ./ ./_build/html/epub/fr -D language=fr
 exit 0
 

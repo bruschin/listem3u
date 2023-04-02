5e75a398ebb162e6c2a1574dba20cc9f47bdc772
BRUSCHI Nicolas <nicolas.bruschi@meteo.fr>
2023-04-02 13:51:47 +0200

    Test 2

--- devtools/lanceur_sphinx.bash
+++ devtools/lanceur_sphinx.bash
@@ -11,8 +11,36 @@ make -C ./ clean
 #sphinx-build . _build
 sphinx-apidoc -f -o ./ ../src
 #sphinx-multiversion ./ _build/html
-sphinx-build -b html ./ ./_build/html -D language=fr
-#sphinx-build -b rinoh ./ ./_build/rinoh -D language=fr
-sphinx-build -b epub ./ ./_build/html/epub/fr -D language=fr
+languages="en $(find ./locales/ -mindepth 1 -maxdepth 1 -type d \
+                -exec basename '{}' \;)"
+
+# make the current version = main available to conf.py
+#current_version="main"
+export current_version="main"
+
+echo "INFO: Building sites for ${current_version}"
+
+for current_language in ${languages}; do
+
+  # make the current language available to conf.py
+  export current_language
+
+  ##########
+  # BUILDS #
+  ##########
+  echo "INFO: Building for ${current_language}"
+
+  # HTML #
+  sphinx-build  -b "html" "./" "./_build/html/${current_language}/"\
+"${current_version}" -D language="${current_language}"
+
+  # EPUB #
+  sphinx-build -b "epub" "./" "./_build/html/${current_language}/"\
+"${current_version}/epub" -D language="${current_language}"
+
+  # PDF #
+  #sphinx-build -b rinoh ./ ./_build/html/rinoh/fr -D language=fr
+done
+
 exit 0
 

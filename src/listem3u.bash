0c6a262bac0b5df7c0cd51249ba95fe6532f94be
BRUSCHI Nicolas <nicolas.bruschi@meteo.fr>
2023-04-24 17:36:07 +0200

    24/04

--- src/listem3u.bash
+++ src/listem3u.bash
@@ -5,16 +5,14 @@
 # par lecture des fichiers m3u dans les sous répertoires
 # https://github.com/koalaman/shellcheck
 #################
-Version="[BN 12-04-2023 V1.2]" # shellcheck
-Repertoire_travail="p:\Morceaux_choisis"
+Version="[BN 24-04-2023 V1.3]"
+#Repertoire_travail="p:\Morceaux_choisis"
+Repertoire_travail="/home/bruschin/workspace/listem3u/automation"
 Fichier_liste_tampon="liste.m3u"
 Fichier_liste="liste-$(date "+%d-%m-%Y").m3u"
 
-cd "${Repertoire_travail}"
-if test $? -ne 0; then
-	echo "${Repertoire_travail} inexistant"
-	exit 1
-fi
+cd "${Repertoire_travail}" || { echo "${Repertoire_travail} inexistant"; \
+                                exit 1; }
 
 if test -f "${Fichier_liste_tampon}"; then
 	rm -f "${Fichier_liste_tampon}"
@@ -23,15 +21,22 @@ if test -f  "${Fichier_liste}"; then
 	rm -f "${Fichier_liste}"
 fi
 
-ficm3u=$(find . -mindepth 1 -maxdepth 2 -type f -print | grep "Playlist.m3u$")
+#ficm3u=$(find . -mindepth 1 -maxdepth 2 -type f -print | grep "Playlist.m3u$")
+ficm3u=$(find . -mindepth 1 -maxdepth 2 -type f -name "*Playlist.m3u")
 #echo "test ${ficm3u}"
 for fic in ${ficm3u}; do
-	cat "${fic}" | LC_COLLATE=C sort --ignore-case >> "${Fichier_liste_tampon}"
-	echo -e "\n" >> "${Fichier_liste_tampon}"
+    cat "${fic}" >> "${Fichier_liste_tampon}"
+	#cat "${fic}" | LC_COLLATE=C sort --ignore-case >> "${Fichier_liste_tampon}"
+	#echo -e "\n" >> "${Fichier_liste_tampon}"
 done
-cat  "${Fichier_liste_tampon}" | \
-grep -Ev -e "^[[:blank:]]*(#|$)" | \
-LC_COLLATE=C sort --ignore-case > "${Fichier_liste}"
+{
+    tampon=$(grep -Ev -e "^[[:blank:]]*(#|$)" "${Fichier_liste_tampon}")    
+    echo "${tampon}" | LC_COLLATE=C sort
+} > "${Fichier_liste}"
+
+#cat "${Fichier_liste_tampon}" | \
+ #   grep -Ev -e "^[[:blank:]]*(#|$)" | \
+ #   LC_COLLATE=C sort --ignore-case > "${Fichier_liste}"
 #sleep 4
 rm -f  "${Fichier_liste_tampon}"
 exit 0

fc33ee88061438e43cab71fab43d374f6e79a327
Nicolas Bruschi <matelotte@hotmail.com>
2026-03-01 15:35:08 +0100

    01-03-2026

--- tests/test_listem3u.py
+++ tests/test_listem3u.py
@@ -31,7 +31,7 @@ from src.listem3u import \
 CWD = os.getcwd()
 REPERTOIRE = os.path.join(CWD, "automation")
 FICCTRL = os.path.join(REPERTOIRE,"000-liste-01-03-2026_ctrl.m3u")
-NBRFICMP3 = 1207
+NBRFICMP3 = 1208
 
 
 # turns all warnings into errors for this module
